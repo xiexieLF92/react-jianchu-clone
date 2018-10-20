4bf556604451e52a5995e6325298dc47cdfd9b1a
xinyixue666 <xinyixue666@163.com>
2018-10-20 10:32:31 +0800

    初始化项目

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import GolbalStyle from "./GlobalStyle";
+import App from './App';
+
+ReactDOM.render(
+<Fragment>
+  <App />
+  <GolbalStyle />
+</Fragment>
+, document.getElementById('root'));
