6109f30234e1a0a43e30780d425db5291f4be624
xinyixue666 <xinyixue666@163.com>
2018-10-23 18:11:18 +0800

    使用react-redux全局注册store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import GolbalStyle from "./GlobalStyle";
+import IconFont from "./static/iconfont/iconfont";
 import App from './App';
+import { Provider } from "react-redux";
+import store from "./store";
 
 ReactDOM.render(
-<Fragment>
-  <App />
-  <GolbalStyle />
-</Fragment>
+<Provider store={store}>
+  <Fragment>
+    <App />
+    <GolbalStyle />
+    <IconFont />
+  </Fragment>
+</Provider>
 , document.getElementById('root'));
