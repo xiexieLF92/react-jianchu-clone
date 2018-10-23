62a7cd20ae7d1a5fd0fd8da30f871a255f6df349
xinyixue666 <xinyixue666@163.com>
2018-10-23 18:10:32 +0800

    使用header组件

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
+import Header from "./common/header";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p></p>
-        </header>
+        <Header></Header>
       </div>
     );
   }
