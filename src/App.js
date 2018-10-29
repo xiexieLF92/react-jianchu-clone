72b0fd11544784b38ddd8d3e93eda2f66a02c8a0
xinyixue666 <xinyixue666@163.com>
2018-10-29 17:16:41 +0800

    增加路由功能

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./common/header";
+import Home from "./pages/home"
+import AsideTools from "./common/asideTools/";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header></Header>
+        <Header />
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact component={Home}></Route>
+            <Route path="/detail" exact render={() => (<div>detail</div>)}></Route>
+          </div>
+        </BrowserRouter>
+        <AsideTools/> 
       </div>
     );
   }
