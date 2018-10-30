3104a3676337f489139e8c269f277ce3335968b1
xinyixue666 <xinyixue666@163.com>
2018-10-30 16:17:02 +0800

    轮播图增加左右点击按钮以及指示块

--- src/pages/home/CarouseBanner/index.js
+++ src/pages/home/CarouseBanner/index.js
@@ -1,27 +1,72 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import {
-  CarouselBannerWrapper
+  CarouselBannerWrapper,
+  CarouselBannerInner,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselIndicatorsItem
 } from "./style";
 
 class CarouselBanner extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bannerIndex: 1,
+      timer: null
+    }
+  }
 
   render() {
     const { carouselList } = this.props;
-
     return(
-      <CarouselBannerWrapper>
-        {
-          carouselList.toJS().map((item, index) => (
-            <div className="banner-item" key={index}>
-              <a href="true">
-                <img src={item.imgSrc} alt=""/>
-              </a>
-            </div>
-          ))
-        }
+      <CarouselBannerWrapper id="carouselBannerWrapper">
+        <CarouselBannerInner ref={(ref) => {this.innerRef = ref}}>
+          {
+            carouselList.toJS().map((item, index) => (
+              <div className="banner-item" key={index}>
+                <a href="true">
+                  <img src={item.imgSrc} alt=""/>
+                </a>
+              </div>
+            ))
+          }
+        </CarouselBannerInner>
+        <CarouselControl className="left-control carousel-control">
+          <i className="iconfont">&#xe640;</i>
+        </CarouselControl>
+        <CarouselControl className="right-control carousel-control">
+          <i className="iconfont">&#xe63f;</i>
+        </CarouselControl>
+        <CarouselIndicators className="carousel-indicators">
+          {
+            carouselList.toJS().map((item, index) => (
+              <CarouselIndicatorsItem 
+                key={index}
+                data-target="#carouselBannerWrapper"
+                data-index-to={index}
+                onClick={e => {this.handleCarouselIndicatorsItem(e)}}
+                >
+              </CarouselIndicatorsItem>
+            ))
+          }
+        </CarouselIndicators>
       </CarouselBannerWrapper>
     );
+  }
+
+  componentDidMount() {
+    this.setCarouselInnerHeight()
+  }
+  setCarouselInnerHeight() {
+    let el = document.querySelector(".banner-item");
+    this.innerRef.style.height = `${el.offsetHeight}px`;
+    console.log(this.innerRef.offsetWidth)
+  }
+  handleCarouselIndicatorsItem(e) {
+    let target = e.target;
+    let index = target.dataset.indexTo;
+    let id = target.dataset.target;
   }
 }
 
--- src/pages/home/CarouseBanner/style.js
+++ src/pages/home/CarouseBanner/style.js
@@ -1,13 +1,30 @@
 import styled from "styled-components";
 
 export const CarouselBannerWrapper = styled.div`
+  position: relative;
+  width: 100%;
   border-radius: 6px;
   margin-bottom: 35px;
   font-size: 0;
-  width: 100%;
+  .carousel-control {
+    display: none;
+  }
+  .carousel-indicators {
+    display: none;
+  }
+  &:hover {
+    .carousel-control {
+      display: block;
+    }
+    .carousel-indicators {
+      display: block;
+    }
+  }
+`
+export const CarouselBannerInner = styled.div`
   position: relative;
+  width: 100%;
   .banner-item {
-    float: left;
     position: absolute;
     a {
       display: block;
@@ -17,4 +34,53 @@ export const CarouselBannerWrapper = styled.div`
       }
     }
   }
+  &:after {
+    height: 0;
+    content: "";
+    visibility: hiden;
+    clear: both;
+  }
+`
+
+export const CarouselControl = styled.div`
+  position: absolute;
+  top: 40%;
+  background-color: rgba(0,0,0,.4);
+  height: 50px;
+  width: 40px;
+  font-size: 20px;
+  color: #fff;
+  text-align: center;
+  cursor: pointer;
+  .iconfont {
+    display: inline-block;
+    line-height: 50px;
+  }
+  &.left-control {
+    left: 0;
+    border-top-right-radius: 6px;
+    border-bottom-right-radius: 6px;
+  }
+  &.right-control {
+    right: 0;
+    border-top-left-radius: 6px;
+    border-bottom-left-radius: 6px;
+  }
+`
+export const CarouselIndicators = styled.ol`
+  position: absolute;
+  width: 80%;
+  padding: 10px 0;
+  left: 10%;
+  bottom: 20px;
+  text-align: center;
+`
+export const CarouselIndicatorsItem = styled.li`
+  display: inline-block;
+  width: 25px;
+  height: 3px;
+  margin: 0 4px;
+  background-color: hsla(0,0%,47%,.4);
+  transition: all .3s ease-in;
+  cursor: pointer;
 `
