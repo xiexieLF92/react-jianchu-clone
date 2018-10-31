ccbb0c52157caac9c651e24f4172ae7db6fb249d
xinyixue666 <xinyixue666@163.com>
2018-10-31 09:13:38 +0800

    优化store

--- src/common/header/store/action.js
+++ src/common/header/store/action.js
@@ -1,41 +1,45 @@
-import { SEARCH_ON_FOCUS, SEARCH_ON_BLUR, SEARCH_ON_MOVE_IN, SEARCH_ON_MOVE_OUT, TOGGLE_POPOVER_MODEL_SHOW, INTI_SRAECH_TREND_LIST, CHANGE_SRAECH_TREND_LIST_PAGENUM } from "./actionTypes";
+import { headerActionTypes } from "./index";
 import axios from "axios";
 
 export const searchOnFocus = () => ({
-  type: SEARCH_ON_FOCUS
+  type: headerActionTypes.SEARCH_ON_FOCUS
 })
 
 export const searchOnBlur = () => ({
-  type: SEARCH_ON_BLUR,
+  type: headerActionTypes.SEARCH_ON_BLUR,
 })
 
 export const searchOnMoveIn = () => ({
-  type: SEARCH_ON_MOVE_IN
+  type: headerActionTypes.SEARCH_ON_MOVE_IN
 })
 
 export const searchOnMoveOut = () => ({
-  type: SEARCH_ON_MOVE_OUT
+  type: headerActionTypes.SEARCH_ON_MOVE_OUT
 })
 
 export const togglePopoverModelShow = () => ({
-  type: TOGGLE_POPOVER_MODEL_SHOW
+  type: headerActionTypes.TOGGLE_POPOVER_MODEL_SHOW
 })
 
-export const initSearchTrendList = (data) => ({
-  type: INTI_SRAECH_TREND_LIST,
+const initSearchTrendList = (data) => ({
+  type: headerActionTypes.INTI_SRAECH_TREND_LIST,
   data,
   totalPage: Math.ceil(data.length/10)
 })
 
 export const changeSearchTrendList = (pagenum) => ({
-  type: CHANGE_SRAECH_TREND_LIST_PAGENUM,
+  type: headerActionTypes.CHANGE_SRAECH_TREND_LIST_PAGENUM,
   pagenum
 })
 
 export const getSearchTrendList = () => {
   return (dispatch) => {
     axios.get('/api/header/searchTrendList.json').then(res => {
-      dispatch(initSearchTrendList(res.data))
+      if(res.data && res.data.success){
+        dispatch(initSearchTrendList(res.data.data.searchTrendList))
+      } else {
+        throw new Error("数据获取失败")
+      }
     }).catch(err => {
       console.log(err)
     })
