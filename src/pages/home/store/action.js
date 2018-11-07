aadd7a10433d4ce10c8e63a32783ff06ce91eb0e
xinyixue666 <xinyixue666@163.com>
2018-11-07 10:12:57 +0800

    优化store

--- src/pages/home/store/action.js
+++ src/pages/home/store/action.js
@@ -1,4 +1,5 @@
 import { homeActionTypes } from "./index";
+import axios from "axios";
 
 export const bigQrCodeShow = () => ({
   type: homeActionTypes.BIG_QRCODE_SHOW
@@ -8,7 +9,43 @@ export const bigQrCodeHidden = () => ({
   type: homeActionTypes.BIG_QRCODE_HIDDEN
 })
 
-export const recomAuthorsListChange = (pagenum) => ({
+export const recomAuthorsListChange = (pageNum) => ({
   type: homeActionTypes.RECOM_AUTHORS_LSIT_CHANGE,
-  pageNum: pagenum
+  pageNum
 })
+
+const initArticleList = (data) => ({
+  type: homeActionTypes.INIT_ARTICLE_LIST,
+  data
+})
+
+export const getArticleList = () => {
+  return (dispatch) => {
+    axios.get("/api/home/articleList.json").then(res => {
+      if(res.data.success) {
+        dispatch(initArticleList(res.data.data))
+      } else {
+        throw new Error("获取数据失败")
+      }
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+}
+
+const initRecomAuthor = (data) => ({
+  type: homeActionTypes.INIT_RECOM_AUTHOR,
+  data
+})
+
+export const getRecomAuthor = () => {
+  return (dispatch) => {
+    axios.get("/api/home/recomAuthor.json").then(res => {
+      if(res.data.success) {
+        dispatch(initRecomAuthor(res.data.data))
+      }
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+}
--- src/pages/home/store/actionTypes.js
+++ src/pages/home/store/actionTypes.js
@@ -1,3 +1,5 @@
 export const BIG_QRCODE_SHOW = "home/big_qrcode_show";
 export const BIG_QRCODE_HIDDEN = "home/big_qrcode_hidden";
 export const RECOM_AUTHORS_LSIT_CHANGE = "recom_authors_list_change";
+export const INIT_ARTICLE_LIST = "init_article_list";
+export const INIT_RECOM_AUTHOR = "init_recom_author";
