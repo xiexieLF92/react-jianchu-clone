90afe2c8359d5abebb36e40c8628e4c46689326c
xinyixue666 <xinyixue666@163.com>
2018-11-09 15:31:10 +0800

    优化路由显示

--- src/common/indexPages/index.js
+++ src/common/indexPages/index.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import { Route } from "react-router-dom";
+import { Route, withRouter } from "react-router-dom";
 
 import Header from "../header";
 import Home from "../../pages/home";
@@ -11,7 +11,7 @@ class IndexPages extends Component {
   render() {
     return (
       <Fragment>
-        <Header />
+        <Route component={Header} />
         <Route path="/" exact component={Home}></Route>
         <Route path="/detail" exact render={() => (<div>detail</div>)}></Route>
         <Route path="/download_apps" exact component={DownloadApps}></Route>
@@ -22,4 +22,4 @@ class IndexPages extends Component {
   }
 }
 
-export default IndexPages;
+export default withRouter(IndexPages);
